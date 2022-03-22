9ed58670873751eb933b2e10d8338099faea3b79
Joel Lee <joel@joellee.org>
2022-03-22 23:00:10 +0100

    feat: add more util methods

--- zetamarkets/exchange.py
+++ zetamarkets/exchange.py
@@ -1,12 +1,26 @@
+from zetamarkets import utils
+
+
 class Exchange:
+    is_initialized = False
+    _mint_authority = None
+    _state = None
+    _serum_authority = None
+
     def __init__(self, program_id, network, connection, wallet):
         pass
 
-    def program():
-        pass
+    @classmethod
+    def load(cls, program_id, network, connection, opts, wallet, throttle_ms):
+        print("Loading Exchange")
+        if Exchange.is_initialized:
+            raise ("Exchange already initialized")
+        [mint_authority, _mint_authority_nonce] = utils.get_mint_authority(program_id)
+        [state, _state_nonce] = utils.get_state(program_id)
+        [serum_authority, _serum_nonce] = utils.get_serum_authority(program_id)
+        _mint_authority = mint_authority
+        _state = state
+        _serum_authority = serum_authority
 
-    def load(self, program_id, network, connection, opts, wallet, throttle_ms):
-        pass
-
-    def initialize_zeta_group():
-        pass
+        # Load Zeta Group
+        [underlying, _underlying_nonce] = utils.get_underlying(program_id, 0)
--- zetamarkets/utils.py
+++ zetamarkets/utils.py
@@ -1,4 +1,5 @@
 from anchorpy import Idl, Program
+import solana
 from typing import Dict
 
 
@@ -8,3 +9,80 @@ def default_commitment() -> Dict:
         "preflightCommitment": "confirmed",
         "commitment": "confirmed",
     }
+
+
+def get_vault(program_id, zeta_group):
+    return solana.publickey.PublicKey.find_program_address(
+        [bytes("vault", "utf-8"), bytes(zeta_group)], program_id
+    )
+
+
+def get_serum_vault(program_id: solana.publickey.PublicKey, zeta_group):
+    """_summary_
+
+    Parameters
+    ----------
+    program_id : _type_
+            _description_
+    zeta_group : _type_
+            _description_
+    """
+    pass
+
+
+def get_mint_authority(program_id: solana.publickey.PublicKey):
+    return solana.publickey.PublicKey.find_program_address(
+        [bytes("mint-auth", "utf-8")], program_id
+    )
+
+
+def get_serum_authority(program_id: solana.publickey.PublicKey):
+    return solana.publickey.PublicKey.find_program_address(
+        [bytes("serum", "utf-8")], program_id
+    )
+
+
+def get_zeta_group(
+    program_id: solana.publickey.PublicKey, mint: solana.publickey.PublicKey
+):
+    """_summary_
+
+    Parameters
+    ----------
+    program_id : solana.publickey.PublicKey
+            _description_
+    mint : solana.publickey.PublicKey
+            _description_
+
+    Returns
+    -------
+    _type_
+            _description_
+    """
+    return solana.publickey.PublicKey.find_program_address(
+        [bytes("zeta-group", "utf-8"), mint], program_id
+    )
+
+
+def get_zeta_vault(program_id, mint):
+    pass
+
+
+def get_user_white_list_deposit_account():
+    pass
+
+
+def get_state(program_id):
+    return solana.publickey.PublicKey.find_program_address(
+        [bytes("state", "utf-8")], program_id
+    )
+
+
+def get_market_uninitialized():
+    pass
+
+
+def get_underlying(program_id, underlying_index: int):
+    return solana.publickey.PublicKey.find_program_address(
+        [bytes("underlying", "utf-8"), bytes([underlying_index])], program_id
+    )
